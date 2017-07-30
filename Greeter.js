55d41992b36c19a99894482321d4523df000612c
Shaii Ong <monsteronfire@gmail.com>
2017-07-30 18:40:51 +0800

    Setup Greeter.init

--- Greeter.js
+++ Greeter.js
@@ -1,3 +1,20 @@
 (function(global, $) {
 
+  var Greeter = function(firstname, lastname, language) {
+    return new Greeter.init(firstname, lastname, language);
+  };
+
+  Greeter.prototype = {};
+
+  Greeter.init = function(firstname, lastname, language) {
+    var self = this;
+    self.firstname = firstname || '';
+    self.lastname = lastname || '';
+    self.language = labguage || 'en';
+  };
+
+  Greeter.init.prototype = Greeter.prototype;
+
+  global.Greeter = global.$G = Greeter;
+
 }(window, jQuery);
