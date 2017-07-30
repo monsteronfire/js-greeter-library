187a40a80eb1d97ee8a51d0320265e88f445395b
Shaii Ong <monsteronfire@gmail.com>
2017-07-30 23:23:14 +0800

    Add private values

--- Greeter.js
+++ Greeter.js
@@ -4,6 +4,23 @@
     return new Greeter.init(firstname, lastname, language);
   };
 
+  var supportedLangs = ['en', 'fj'];
+
+  var greetings = {
+    en: 'Hello ',
+    fj: 'Bula '
+  };
+
+  var formalGreetings = {
+    en: 'Greetings, ',
+    fj: 'Ni Sa Bula, '
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    fj: 'Logged in'
+  };
+
   Greeter.prototype = {};
 
   Greeter.init = function(firstname, lastname, language) {
